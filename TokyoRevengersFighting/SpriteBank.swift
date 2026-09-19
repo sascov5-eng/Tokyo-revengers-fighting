42bfd8b595b13b357fd074b339e156e7acd0911e
sascov5-eng <sascov5@gmail.com>
2026-09-19 07:04:53 +0300

    Lock fighter sprites: blonde Mikey hairColor + robust SpriteBank decoder

--- TokyoRevengersFighting/SpriteBank.swift
+++ TokyoRevengersFighting/SpriteBank.swift
@@ -7,8 +7,10 @@ enum SpriteBank {
     static func texture(_ key: String) -> SKTexture? {
         if let t = cache[key] { return t }
         let b64 = SpriteDataA.blobs[key] ?? SpriteDataB.blobs[key]
-        guard let raw = b64, let data = Data(base64Encoded: raw),
-              let img = UIImage(data: data), let cg = img.cgImage else { return nil }
+        guard let raw = b64,
+              let data = Data(base64Encoded: raw, options: .ignoreUnknownCharacters),
+              let img = UIImage(data: data),
+              let cg = img.cgImage else { return nil }
         let t = SKTexture(cgImage: cg)
         t.filteringMode = .nearest
         cache[key] = t
