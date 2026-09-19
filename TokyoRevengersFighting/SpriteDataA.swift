f7b125ab7618acaaaf8f17be54d6428f86297089
sascov5-eng <sascov5@gmail.com>
2026-09-19 06:39:15 +0300

    phase 3: sprite data stubs and pbx entries so the project compiles

---
+++ TokyoRevengersFighting/SpriteDataA.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+enum SpriteDataA {
+    static let blobs: [String: String] = [:]
+}
---
+++ TokyoRevengersFighting/SpriteDataB.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+enum SpriteDataB {
+    static let blobs: [String: String] = [:]
+}
